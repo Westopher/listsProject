c68ff61331a5dec249304dff1617f33fd4e87a59
West Kraemer <kraemerwest@gmail.com>
2018-07-24 17:02:47 -0400

    more methods added

--- Lists/ListItemsVC.swift
+++ Lists/ListItemsVC.swift
@@ -2,7 +2,7 @@
 
 import UIKit
 
-class ListItemsVC: UIViewController {
+class ListItemsVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var newItemNameTextField: UITextField!
     @IBOutlet weak var itemsTableView: UITableView!
@@ -13,10 +13,31 @@ class ListItemsVC: UIViewController {
         super.viewDidLoad()
         
     }
-
+    // MARK: - TableView DataSource methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let listItemTVCell = tableView.dequeueReusableCell(withIdentifier: "ListItemTVCell", for: indexPath) as! ListItemTVCell
+        listItemTVCell.itemNameLabel.text = list.items[indexPath.row].title
+        return listItemTVCell
+    }
+    
+    // MARK: - TableView Delegate methods
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == . delete {
+            list.items.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    // MARK: - UI responsive methods
     
     @IBAction func addItemButtonTapped(_ sender: Any) {
-        
+        if newItemNameTextField.text == "" { return }
+        list.items.append(ListItem(itemTitle: newItemNameTextField.text!)!)
+        itemsTableView.reloadData()
     }
     
     /*
